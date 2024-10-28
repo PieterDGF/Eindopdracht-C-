f5db09d280c7633c175fa22bbbca51109b0ccdd8
Erik <er.dijkstra@student.avans.nl>
2024-10-28 15:11:38 +0100

    server code

--- Eindopdracht - Server/Program.cs
+++ Eindopdracht - Server/Program.cs
@@ -1,27 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections;
 using System.Net;
 using System.Net.Sockets;
-try
-{
-    TcpListener tcpListener = new TcpListener(IPAddress.Any, 8001);
-    tcpListener.Start();
+using System.Text;
 
-    while (true)
+public class Server()
+{
+    private static ArrayList socketList;
+    private static ASCIIEncoding asen = new ASCIIEncoding();
+
+    public static void Main(string[] args)
     {
-        Socket socket = tcpListener.AcceptSocket();
-        Thread socketThread = new Thread(new ParameterizedThreadStart(newSocket));
-        socketThread.Start();
+        try
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8001);
+            tcpListener.Start();
+
+            socketList = new ArrayList();
+
+
+            while (true)
+            {
+                Socket socket = tcpListener.AcceptSocket();
+                socketList.Add(socket);
+                Thread socketThread = new Thread(new ParameterizedThreadStart(newSocket));
+                socketThread.Start(socket);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
     }
-} catch (Exception e) 
-{ 
-    Console.WriteLine(e.ToString());
-}
 
-void newSocket(Object object1)
-{
-    //socket logt in
-}
+    public static void newSocket(Object object1)
+    {
+        Socket socket = object1 as Socket;
 
+        byte[] b = new byte[100];
+        int k = socket.Receive(b);
+        String name = System.Text.Encoding.ASCII.GetString(b);
+
+        socket.Send(asen.GetBytes("chat history")); //TODO moet nog een chat history worden
+
+        while (true)
+        {
+            b = new byte[100];
+            k = socket.Receive(b);
+            String message = "message |" + name + ": " + System.Text.Encoding.ASCII.GetString(b);
+            sendMessageToAll(socket, message);
+        }
+    }
+
+    public static void sendMessageToAll(Socket socket, String message)
+    {
+        foreach (Socket socket1 in socketList)
+        {
+            if (socket1 != socket)
+            {
+                socket1.Send(asen.GetBytes(message));
+            }
+        }
+    }
+}
 
 
