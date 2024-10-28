af6c18642f92d597690a315584753da93df99279
Erik <er.dijkstra@student.avans.nl>
2024-10-28 15:11:02 +0100

    server push 1

--- Eindopdracht - Server/Program.cs
+++ Eindopdracht - Server/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Net;
+using System.Net.Sockets;
+try
+{
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 8001);
+    tcpListener.Start();
+
+    while (true)
+    {
+        Socket socket = tcpListener.AcceptSocket();
+        Thread socketThread = new Thread(new ParameterizedThreadStart(newSocket));
+        socketThread.Start();
+    }
+} catch (Exception e) 
+{ 
+    Console.WriteLine(e.ToString());
+}
+
+void newSocket(Object object1)
+{
+    //socket logt in
+}
+
+
+
